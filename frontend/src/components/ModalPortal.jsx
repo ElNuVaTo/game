2340117384437c3417ee28236192677b076e7c6f
Angelo <amaroalcaq23@gmail.com>
2024-11-25 06:24:03 -0300

    fix: corrected file path

--- frontend/src/components/ModalPortal.jsx
+++ frontend/src/components/ModalPortal.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
-import bgModal from "@/assets/bg5.png";
+import bgModal from "@/assets/bg/bg5.png";
 
 const ModalPortal = ({ children, open, data, variants, onClose }) => {
   useEffect(() => {
@@ -36,7 +36,7 @@ const ModalPortal = ({ children, open, data, variants, onClose }) => {
             className="bg-[#111010] p-6 rounded-lg shadow-md relative w-[300px] h-[300px]"
           >
             <div
-            className="absolute top-0 left-0 rounded-lg"
+              className="absolute top-0 left-0 rounded-lg"
               style={{
                 width: "100%",
                 height: "100%",
