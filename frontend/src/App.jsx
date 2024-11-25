60f25f78e5eb7ceeb79bc0ea62c95dd9ca16cd86
Angelo <amaroalcaq23@gmail.com>
2024-11-25 10:18:28 -0300

    feat(path): update and optimize path definitions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Pages
-import Invited from "@/pages/auth/Invited";
-import AuthJoin from "@/pages/auth/AuthJoin";
-import AuthRegister from "@/pages/auth/AuthRegister";
-import Room from "@/pages/room/Room";
-
+// Layouts
 import Bg from "./layout/bg/Bg";
 import Nav from "./layout/navegacion/Nav";
 import AuthNav from "./layout/auth-nav/AuthNav";
+
+// Pages
+import Guest from "@/pages/auth/Guest";
+import Login from "@/pages/auth/Login";
+import Register from "@/pages/auth/Register";
+
+import Room from "@/pages/room/Room";
 
 const App = () => {
   return (
@@ -17,10 +19,10 @@ const App = () => {
         <Bg />
 
         <Routes>
-          <Route element={<AuthNav/>}>
-            <Route path="" index element={<Invited />} />
-            <Route path="login" element={<AuthJoin />} />
-            <Route path="register" element={<AuthRegister />} />
+          <Route element={<AuthNav />}>
+            <Route path="" index element={<Guest />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Route>
 
           <Route path="room" element={<Nav />}>
